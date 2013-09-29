688d4dcca05db390d25f5d9cfc73ed47a89bcf29
Sho Nakatani <lay.sakura@gmail.com>
2013-09-29 14:27:30 +0900

    事前課題と同じ状態にした Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- lib/todo9/Web.pm
+++ lib/todo9/Web.pm
@@ -4,32 +4,37 @@ use strict;
 use warnings;
 use utf8;
 use Kossy;
+use todo9::DB;
+use todo9::Config;
 
-filter 'set_title' => sub {
-    my $app = shift;
-    sub {
-        my ( $self, $c )  = @_;
-        $c->stash->{site_name} = __PACKAGE__;
-        $app->($self,$c);
+
+get '/' => sub {
+    my ( $self, $c )  = @_;
+
+    my $db = todo9::DB::read_memo();
+    $c->render('index.tx',
+               {
+                   memo_content => $db->{content},
+                   last_update => $db->{last_update},
+               });
+};
+
+post '/' => sub {
+    my ( $self, $c ) = @_;
+
+    my $form = $c->req->validator([
+        'memo-area' => {
+            'rule' => [],
+        }]);
+    eval {
+        todo9::DB::save_memo($form->valid('memo-area'));
+        $c->redirect('/');
+    };
+    if ($@) {
+        print $@;
+        $c->res->status(500);
     }
-};
-
-get '/' => [qw/set_title/] => sub {
-    my ( $self, $c )  = @_;
-    $c->render('index.tx', { greeting => "Hello" });
-};
-
-get '/json' => sub {
-    my ( $self, $c )  = @_;
-    my $result = $c->req->validator([
-        'q' => {
-            default => 'Hello',
-            rule => [
-                [['CHOICE',qw/Hello Bye/],'Hello or Bye']
-            ],
-        }
-    ]);
-    $c->render_json({ greeting => $result->valid->get('q') });
+    return $c->res;
 };
 
 1;
